23c25235f90ceced4ee26ce28ece70671cf38f09
Glenn Turner <glenn.turner@wolterskluwer.com>
2018-05-02 17:21:26 +0100

    Client list and send page

--- DocDoctors/Controllers/DocumentController.cs
+++ DocDoctors/Controllers/DocumentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DocDoctors.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -27,8 +28,9 @@ namespace DocDoctors.Controllers
 
         // POST api/<controller>
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Client(Client client)
         {
+            return View();
         }
 
         // PUT api/<controller>/5

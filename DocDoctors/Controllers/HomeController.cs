0049b5c1336e8d3f03ba8aafe91a6db944811b9a
Glenn Turner <glenn.turner@wolterskluwer.com>
2018-05-02 13:59:21 +0100

    Add web api, dal and model project and client page

--- DocDoctors/Controllers/HomeController.cs
+++ DocDoctors/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DocDoctors.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DocDoctors.DAL;
 
 namespace DocDoctors.Controllers
 {
@@ -12,7 +14,35 @@ namespace DocDoctors.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["CompanyName"] = "Document Doctors Ltd.";
+
+            //List<Client> clients = new List<Client>();
+            //clients.Add(new Client() { ClientId = 1, Name = "Glenn" });
+            //clients.Add(new Client() { ClientId = 2, Name = "Dan" });
+            ClientGateway clientGateway = new ClientGateway();
+            ClientCollection clients = clientGateway.GetClients();
+
+            IEnumerable<SelectListItem> items = new List<SelectListItem>();
+            items = clients.Select(c => new SelectListItem
+            {
+                Value = c.ClientId.ToString(),
+                Text = c.Name
+            });
+
+            //List<SelectListItem> items = new List<SelectListItem>();
+            //items.Add(new SelectListItem { Text = "MyId1", Value = "MyId1", Selected = true });
+            //items.Add(new SelectListItem { Text = "MyId2", Value = "MyId2" });
+            //items.Add(new SelectListItem { Text = "MyId3", Value = "MyId3" });
+            ViewBag.ListOfClients = items;
+
             return View();
+        }
+
+        public IActionResult Client()
+        {
+            ClientGateway clientGateway = new ClientGateway();
+            ClientCollection clients = clientGateway.GetClients();
+            return View("Client", clients);
         }
 
         public IActionResult About()
